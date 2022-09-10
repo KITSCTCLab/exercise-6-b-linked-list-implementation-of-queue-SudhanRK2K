eb1f473342941fef9ec31db9ff1744e0c88e4ac6
SudhanRK2K <110010355+SudhanRK2K@users.noreply.github.com>
2022-09-10 09:13:35 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -27,8 +27,7 @@ for specific_operation in input().split(','):
 input_data = input()
 data = input_data.split(',')
 for i in range(len(operations)):
-  if operations[i] == "enqueue":
-    queue.enqueue(int(data[i]))
+  i[i]))
   elif operations[i] == "dequeue":
     queue.dequeue()
 queue.sta
